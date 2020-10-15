6c7fe2f2c4e20219eafa44c0608f535121cf297e
Wade Abel <thepassivetrust@gmail.com>
2020-10-15 13:51:50 -0700

    Updates

---
+++ precache-manifest.33f257324d9bcb4356cebc57e304a2a4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc37f1ff1c660ecb6a8c008521d2273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f655c781f5efad810b9",
+    "url": "/static/css/main.92305edc.chunk.css"
+  },
+  {
+    "revision": "e07fd6b514d7346f2b17",
+    "url": "/static/js/2.ec0f614b.chunk.js"
+  },
+  {
+    "revision": "3f655c781f5efad810b9",
+    "url": "/static/js/main.feb5c354.chunk.js"
+  },
+  {
+    "revision": "3bae2dca38fca1374839",
+    "url": "/static/js/runtime~main.afc96264.js"
+  },
+  {
+    "revision": "fd01eab21357cdff543e171802cb716a",
+    "url": "/static/media/AttestationMaven.fd01eab2.jpg"
+  },
+  {
+    "revision": "2c4459a0b11928bda41b928cfcaf0371",
+    "url": "/static/media/FounderValidator.2c4459a0.jpg"
+  },
+  {
+    "revision": "60e4e3b5ce45777055800399a0f41ca0",
+    "url": "/static/media/MasterValidator.60e4e3b5.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "de1258c4bbed5e67040b0adc10067130",
+    "url": "/static/media/hands.de1258c4.jpg"
+  },
+  {
+    "revision": "e025edea1f0973d0c7bbfeb80c1679a6",
+    "url": "/static/media/mina_background.e025edea.png"
+  },
+  {
+    "revision": "da8208a36d336a98cde583a089b9ed54",
+    "url": "/static/media/mina_logo.da8208a3.svg"
+  },
+  {
+    "revision": "fc07a2a72392900defeddbb67d9e3932",
+    "url": "/static/media/near_group.fc07a2a7.png"
+  },
+  {
+    "revision": "4575f570dc350628818ca57e8b09eec4",
+    "url": "/static/media/near_logo.4575f570.svg"
+  },
+  {
+    "revision": "c997e1c72a322486bf4394c2dd44eeb0",
+    "url": "/static/media/networks.c997e1c7.jpg"
+  },
+  {
+    "revision": "7c38bf97a7978b084948a0f2cd27850a",
+    "url": "/static/media/peeps.7c38bf97.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67c8cdb609509865afff1f5d8f0ada0f.js"
+  "/precache-manifest.33f257324d9bcb4356cebc57e304a2a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
